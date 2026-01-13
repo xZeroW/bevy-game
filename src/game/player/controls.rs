d0fab3858aef2e2702d592342f60176daae0bbf3
xZeroW <victordmartins@protonmail.com>
2026-01-13 18:33:40 -0300

    feat: flip sprite

--- src/game/player/controls.rs
+++ src/game/player/controls.rs
@@ -1,13 +1,15 @@
-use bevy::prelude::{Query, Res, Time, Transform, ButtonInput, KeyCode};
+use bevy::{prelude::{ButtonInput, KeyCode, Query, Res, Time, Transform}, sprite::Sprite};
 
 use crate::game::common::components::characters::position::Position;
 
-pub fn controls(input: Res<ButtonInput<KeyCode>>, time: Res<Time>, mut query: Query<(&mut Position, &mut Transform)>) {
-    for (mut pos, mut transform) in &mut query {
+pub fn controls(input: Res<ButtonInput<KeyCode>>, time: Res<Time>, mut query: Query<(&mut Position, &mut Sprite)>) {
+    for (mut pos, mut sprite) in &mut query {
         if input.pressed(KeyCode::ArrowRight) || input.pressed(KeyCode::KeyD) {
+            sprite.flip_x = false;
             pos.x += 300.0 * time.delta_secs();
         }
         if input.pressed(KeyCode::ArrowLeft) || input.pressed(KeyCode::KeyA) {
+            sprite.flip_x = true;
             pos.x -= 300.0 * time.delta_secs();
         }
         if input.pressed(KeyCode::ArrowUp) || input.pressed(KeyCode::KeyW) {
