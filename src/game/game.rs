f13c452c47be6a0e8c83c90552fee98b1e3bb3a9
xZeroW <victordmartins@protonmail.com>
2026-01-13 17:56:05 -0300

    feat: move mostly to game.rs

--- src/game/game.rs
+++ src/game/game.rs
@@ -1,11 +1,32 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, remote::{RemotePlugin, http::RemoteHttpPlugin}};
 
 use crate::game::player::player::PlayerPlugin;
+use crate::game::player::component::Player;
+use crate::game::common::components::characters::{move_speed::MoveSpeed, position::Position, state::State};
 
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(PlayerPlugin);
+        app.add_plugins(PlayerPlugin)
+        .insert_resource(ClearColor(Color::srgb(0.4, 0.35, 0.45)))
+        .add_plugins(RemotePlugin::default())
+        .add_plugins(RemoteHttpPlugin::default())
+        .add_systems(Startup, setup);
     }
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+
+    commands.spawn(Player {
+        sprite:Sprite::from_image(asset_server.load("classes/archer.png")),
+        position: Position::default(),
+        move_speed: MoveSpeed::default(),
+        state: State::default(),
+    });
+
+    commands.spawn((
+        Sprite::from_image(asset_server.load("classes/default.png")),
+    ));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use bevy::{prelude::*, remote::{RemotePlugin, http::RemoteHttpPlugin}};
+use bevy::prelude::*;
 
 pub mod game;
 
-use crate::game::player::component::Player;
-use crate::game::common::components::characters::{move_speed::MoveSpeed, position::Position, state::State};
 use crate::game::game::GamePlugin;
 
 fn main() {
@@ -19,25 +17,6 @@ fn main() {
                 })
                 .set(ImagePlugin::default_nearest()),
         )
-        .insert_resource(ClearColor(Color::srgb(0.4, 0.35, 0.45)))
-        .add_plugins(RemotePlugin::default())
-        .add_plugins(RemoteHttpPlugin::default())
         .add_plugins(GamePlugin)
-        .add_systems(Startup, setup)
         .run();
-}
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2d);
-
-    commands.spawn(Player {
-        sprite:Sprite::from_image(asset_server.load("classes/archer.png")),
-        position: Position::default(),
-        move_speed: MoveSpeed::default(),
-        state: State::default(),
-    });
-
-    commands.spawn((
-        Sprite::from_image(asset_server.load("classes/default.png")),
-    ));
 }
